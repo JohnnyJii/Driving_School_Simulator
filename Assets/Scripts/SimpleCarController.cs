e464a810a75e20fdca489db6c8a11af855855c45
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2020-05-05 20:28:42 +0300

    TLights

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -1,9 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleCarController : MonoBehaviour
 {
+
+	private float m_horizontalInput;
+	private float m_verticalInput;
+	private float m_steeringAngle;
+	private new Rigidbody rigidbody;
+	public float KPH;
+
+	public Text speedLimit;
+
+	public WheelCollider frontDriverW, frontPassengerW;
+	public WheelCollider rearDriverW, rearPassengerW;
+	public Transform frontDriverT, frontPassengerT;
+	public Transform rearDriverT, rearPassengerT;
+	public float maxSteerAngle = 30;
+	public float motorForce = 50;
+
+	public bool isBrake = false;
+	public float brakeTorque = 15000f;
+
+	public void Start()
+	{
+		getObjects();
+		speedLimit.text = "";
+	}
 
 	public void GetInput()
 	{
@@ -22,6 +47,35 @@ public class SimpleCarController : MonoBehaviour
 	{
 		frontDriverW.motorTorque = m_verticalInput * motorForce;
 		frontPassengerW.motorTorque = m_verticalInput * motorForce;
+		KPH = rigidbody.velocity.magnitude * 3.6f;
+	}
+
+	void HandBrake ()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			isBrake = true;
+		} else
+		{
+			isBrake = false;
+		}
+		if (isBrake == true)
+		{
+			rearDriverW.brakeTorque = brakeTorque;
+			rearPassengerW.brakeTorque = brakeTorque;
+			frontDriverW.brakeTorque = brakeTorque;
+			frontPassengerW.brakeTorque = brakeTorque;
+			rearDriverW.motorTorque = 0;
+			rearPassengerW.motorTorque = 0;
+			frontDriverW.motorTorque = 0;
+			frontPassengerW.motorTorque = 0;
+		} else
+		{
+			rearDriverW.brakeTorque = 0;
+			rearPassengerW.brakeTorque = 0;
+			frontDriverW.brakeTorque = 0;
+			frontPassengerW.brakeTorque = 0;
+		}
 	}
 
 	private void UpdateWheelPoses()
@@ -49,16 +103,24 @@ public class SimpleCarController : MonoBehaviour
 		Steer();
 		Accelerate();
 		UpdateWheelPoses();
+		ExceedingSpeedLimit();
+		HandBrake();
 	}
 
-	private float m_horizontalInput;
-	private float m_verticalInput;
-	private float m_steeringAngle;
+	private void getObjects()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
 
-	public WheelCollider frontDriverW, frontPassengerW;
-	public WheelCollider rearDriverW, rearPassengerW;
-	public Transform frontDriverT, frontPassengerT;
-	public Transform rearDriverT, rearPassengerT;
-	public float maxSteerAngle = 30;
-	public float motorForce = 50;
+	void ExceedingSpeedLimit ()
+	{
+		if (KPH > 40)
+		{
+			speedLimit.text = "You are exceeding the speed limit!";
+			Debug.Log(speedLimit);
+		} else
+		{
+			speedLimit.text = "";
+		}
+	}
 }
---
+++ Assets/TrafficLights.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MYCLASSNAME : MonoBehaviour {
+    
+Light RedLight;
+Light GreenLight;
+Light YellowLight;
+float GreenTimer = 5; // default 5sec.
+float redTimer = 15; // default 15sec.
+ 
+bool  estaVerde = true;
+bool  estaVermelho = true;
+ 
+IEnumerator  Start (){
+ YellowLight.enabled = false;
+ 
+ while(true)
+ {  
+  Verde();
+  yield return new WaitForSeconds(GreenTimer);
+  
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.1f);
+ 
+  YellowLight.enabled = false;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.2f);
+ 
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.3f);
+ 
+  YellowLight.enabled = false;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.4f);
+ 
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.5f);
+ 
+  YellowLight.enabled = false;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.6f);
+ 
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.7f);  
+ 
+  YellowLight.enabled = false;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.8f);  
+ 
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.9f);  
+ 
+  YellowLight.enabled = false;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.10f);
+ 
+  YellowLight.enabled = true;  
+  GreenLight.enabled = false;
+  yield return new WaitForSeconds(0.1f);
+  
+  Vermelho();
+  yield return new WaitForSeconds(redTimer);
+ }
+}
+
+void  Verde (){
+ if (estaVerde)
+ {
+  GreenLight.enabled = true;    
+  RedLight.enabled = false;
+ }
+}
+void  Vermelho (){
+ if (estaVermelho)
+ {
+  RedLight.enabled = true;  
+  YellowLight.enabled = false;
+ }
+}
+ 
+}
